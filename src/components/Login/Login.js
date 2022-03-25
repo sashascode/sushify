f468a65d0253f60a2ac80c99fb23a1b41d9c6909
unknown <racagnisasha@gmail.com>
2022-03-25 00:27:26 -0300

    api auth

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,22 @@
 import { LoginContainer, LoginButton } from "./styles"
 
+const endpoint = 'https://accounts.spotify.com/authorize';
+const clientID = '791d855bdf4e4930a3584a4443cbe6f4';
+const redirectUri = 'http://localhost:3000/';
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state"
+];
+const loginURL = `${endpoint}?client_id=${clientID}&response_type=token&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&show_dialog=true`;
+
 const Login = () => {
   return (
     <LoginContainer>
         <img src="https://1000logos.net/wp-content/uploads/2017/08/Spotify-symbol.jpg" alt="Spotify Logo"/>
-        <LoginButton href="#">Sign in</LoginButton>
+        <LoginButton href={loginURL}>Sign in</LoginButton>
     </LoginContainer>
   )
 }
