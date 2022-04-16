9fbea0964c2a83c156cec37c80ef52318b8f6118
unknown <racagnisasha@gmail.com>
2022-04-15 23:58:17 -0300

    refactored body

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,25 +1,19 @@
-import { BodyContainer, Info, InfoText, Songs, StartPlaylist } from './styles'
+import { BodyContainer } from './styles'
 import { Header } from './Header';
-import { selectPlaylist } from '../../features/PlaylistSlice';
-import { useSelector } from 'react-redux';
-import SongRow from './SongRow';
-import { BsPlayCircleFill, BsPauseCircleFill } from 'react-icons/bs';
-import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useEffect } from 'react';
 import { spotifyApi } from '../../spotifyLogic';
 import { useDispatch } from 'react-redux';
 import { SET_PLAYLIST } from '../../features/PlaylistSlice';
+import { PlaylistBody } from './PlaylistBody';
 
 const Body = ({accesToken}) => {
-  const playlist = useSelector(selectPlaylist);
-  const [playActive, setPlayActive] = useState(false);
   const {playlistId} = useParams();
   const dispatch = useDispatch();
 
   useEffect(() => {
     if(playlistId){
-    spotifyApi.setAccessToken(accesToken)
+    spotifyApi.setAccessToken(accesToken);
     spotifyApi.getPlaylist(playlistId).then(playlist => dispatch(SET_PLAYLIST(playlist.body)));
     return (() => {
       SET_PLAYLIST();
@@ -29,30 +23,8 @@ const Body = ({accesToken}) => {
   
   return (
     <BodyContainer id='scrollbar'>
-
       <Header/>
-      <Info>
-        <img src={playlist?.images[0]?.url} alt={playlist?.name} />
-        <InfoText>
-          <h6>PLAYLIST</h6>
-          <h1>{playlist?.name}</h1>
-          <p>{playlist?.description}</p>
-          <span>{playlist?.owner.display_name} &#8226; {playlist?.followers.total} me gusta &#8226; {playlist?.tracks.total} canciones </span>
-        </InfoText>
-      </Info>
-
-      <StartPlaylist onClick={() => setPlayActive(!playActive)}>
-        {playActive ? <BsPauseCircleFill/> : <BsPlayCircleFill/>}
-      </StartPlaylist>
-
-      <Songs>
-        {
-          playlist?.tracks?.items?.map((song, index) => {
-            return <SongRow track={song?.track} index={index} key={song?.track?.id}/> 
-          })
-        }
-      </Songs>
-
+      {playlistId && <PlaylistBody/>}
     </BodyContainer>
   );
 }
---
+++ src/components/Body/PlaylistBody.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { selectPlaylist } from '../../features/PlaylistSlice';
+import { Info, InfoText, StartPlaylist, Songs } from './styles';
+import SongRow from './SongRow';
+import { BsPlayCircleFill, BsPauseCircleFill } from 'react-icons/bs';
+
+export const PlaylistBody = () => {
+  const playlist = useSelector(selectPlaylist);
+  const [playActive, setPlayActive] = useState(false);
+
+  return (
+    <>
+        <Info>
+            <img src={playlist?.images[0]?.url} alt={playlist?.name} />
+            <InfoText>
+            <h6>PLAYLIST</h6>
+            <h1>{playlist?.name}</h1>
+            <p>{playlist?.description}</p>
+            <span>{playlist?.owner.display_name} &#8226; {playlist?.followers.total} me gusta &#8226; {playlist?.tracks.total} canciones </span>
+            </InfoText>
+        </Info>
+
+        <StartPlaylist onClick={() => setPlayActive(!playActive)}>
+            {playActive ? <BsPauseCircleFill/> : <BsPlayCircleFill/>}
+        </StartPlaylist>
+
+        <Songs>
+            {
+            playlist?.tracks?.items?.map((song, index) => {
+                return <SongRow track={song?.track} index={index} key={song?.track?.id}/> 
+            })
+            }
+        </Songs>
+      </>
+  )
+}
